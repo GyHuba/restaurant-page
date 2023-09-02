194cb6acdd5a2b335a50cef34cca7c05f7e53988
GyHuba <gy.huba@gmail.com>
2023-09-02 17:20:52 +0200

    page changing logic

--- src/loadHeader.js
+++ src/loadHeader.js
@@ -1,5 +1,19 @@
+import pageLoad from "./pageLoad";
+
 export default function loadHeader() {
     const content = document.querySelector('#content');
+
+    function changeHeader(e){
+        if(homeLi.classList.contains('active')) homeLi.classList.remove('active');
+        else if(menuLi.classList.contains('active')) menuLi.classList.remove('active');
+        else if(contactLi.classList.contains('active')) contactLi.classList.remove('active');
+        e.target.classList.add('active');
+
+        content.innerHTML = "";
+        content.append(header)
+
+        pageLoad();
+    }
 
     const header = document.createElement('div');
     header.classList.add('header');
@@ -8,12 +22,15 @@ export default function loadHeader() {
     const headerUl = document.createElement('ul');
     const homeLi = document.createElement('li');
     homeLi.classList.add('active','home');
+    homeLi.addEventListener('click', changeHeader);
     homeLi.innerHTML = 'Home';
     const menuLi = document.createElement('li');
-    menuLi.classList.add('active','menu');
+    menuLi.classList.add('menu');
+    menuLi.addEventListener('click', changeHeader);
     menuLi.innerHTML = 'Menu';
     const contactLi = document.createElement('li');
-    contactLi.classList.add('active','contact');
+    contactLi.classList.add('contact');
+    contactLi.addEventListener('click', changeHeader);
     contactLi.innerHTML = 'Contact';
     headerUl.append(homeLi, menuLi, contactLi);
     header.append(headerTitle, headerUl);
